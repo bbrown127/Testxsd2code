a528535bb791121f4ed907b6e7efe8defa46358f
Brent Browske <bbrown127+github@gmail.com>
2016-09-18 15:53:39 -0500

    Add project files.

---
+++ xsd2code/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xsd2code
+{
+    public class Class1
+    {
+    }
+}
